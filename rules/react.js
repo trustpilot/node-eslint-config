34226ce113a4a3ebb80ded5b372b7ad0fbdb4dd3
Spencer Hakim <sha@trustpilot.com>
2016-02-25 15:01:44 -0500

    Copy default React rule settings from eslint-plugin-react

--- rules/react.js
+++ rules/react.js
@@ -11,7 +11,7 @@ module.exports = {
   },
   "rules": {
     // Prevent missing displayName in a React component definition
-    "react/display-name": 0,
+    "react/display-name": 2,
     // Forbid certain propTypes
     "react/forbid-prop-types": 0,
     // Enforce boolean attributes notation in JSX
@@ -31,13 +31,11 @@ module.exports = {
     // Prevent usage of .bind() and arrow functions in JSX props
     "react/jsx-no-bind": 0,
     // Prevent duplicate props in JSX
-    "react/jsx-no-duplicate-props": 0,
-    // Prevent usage of isMounted
-    "react/no-is-mounted": 0,
+    "react/jsx-no-duplicate-props": 2,
     // Prevent usage of unwrapped JSX strings
     "react/jsx-no-literals": 0,
     // Disallow undeclared variables in JSX
-    "react/jsx-no-undef": 0,
+    "react/jsx-no-undef": 2,
     // Enforce PascalCase for user-defined JSX components
     "react/jsx-pascal-case": 0,
     // Enforce props alphabetical sorting
@@ -45,19 +43,21 @@ module.exports = {
     // Enforce space before closing bracket
     "react/jsx-space-before-closing": 0,
     // Prevent React to be incorrectly marked as unused
-    "react/jsx-uses-react": 0,
+    "react/jsx-uses-react": 2,
     // Prevent variables used in JSX to be incorrectly marked as unused
-    "react/jsx-uses-vars": 0,
+    "react/jsx-uses-vars": 2,
     // Prevent usage of dangerous JSX properties
-    "react/no-danger": 0,
+    "react/no-danger": 2,
     // Prevent usage of deprecated methods
-    "react/no-deprecated": 0,
+    "react/no-deprecated": 2,
     // Prevent usage of setState in componentDidMount
-    "react/no-did-mount-set-state": 0,
+    "react/no-did-mount-set-state": [2, 'allow-in-func'],
     // Prevent usage of setState in componentDidUpdate
-    "react/no-did-update-set-state": 0,
+    "react/no-did-update-set-state": [2, 'allow-in-func'],
     // Prevent direct mutation of this.state
-    "react/no-direct-mutation-state": 0,
+    "react/no-direct-mutation-state": 2,
+    // Prevent usage of isMounted
+    "react/no-is-mounted": 2,
     // Prevent multiple component definition per file
     "react/no-multi-comp": 0,
     // Prevent usage of setState
@@ -65,13 +65,13 @@ module.exports = {
     // Prevent using string references in ref attribute.
     "react/no-string-refs": 0,
     // Prevent usage of unknown DOM property
-    "react/no-unknown-property": 0,
+    "react/no-unknown-property": 2,
     // Enforce ES5 or ES6 class for React Components
     "react/prefer-es6-class": 0,
     // Prevent missing props validation in a React component definition
-    "react/prop-types": 0,
+    "react/prop-types": 2,
     // Prevent missing React when using JSX
-    "react/react-in-jsx-scope": 0,
+    "react/react-in-jsx-scope": 2,
     // Restrict file extensions that may be required
     "react/require-extension": 0,
     // Prevent extra closing tags for components without children
