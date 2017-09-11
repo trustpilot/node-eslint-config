2451a85985727e23535ff8cd93b3725bd255d56d
Mads Godvin Jensen <mje@trustpilot.com>
2017-09-11 10:52:12 +0200

    I don't think we need to declare prop-types on every single component.

--- rules/react.js
+++ rules/react.js
@@ -186,11 +186,7 @@ module.exports = {
 
     // Prevent missing props validation in a React component definition
     // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/prop-types.md
-    'react/prop-types': ['error', {
-      ignore: [],
-      customValidators: [],
-      skipUndeclared: false
-    }],
+    'react/prop-types': ['off'],
 
     // Prevent missing React when using JSX
     // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/react-in-jsx-scope.md
