b08d774df4b35fad5076bc6646469d94433c8e46
Mads Godvin Jensen <mje@trustpilot.com>
2017-12-01 10:31:46 +0100

    Arrow functions are not confusing... (#33)

--- rules/eslint/es6.js
+++ rules/eslint/es6.js
@@ -21,7 +21,7 @@ module.exports = {
     // disallow modifying variables of class declarations
     'no-class-assign': 0,
     // disallow arrow functions where they could be confused with comparisons
-    'no-confusing-arrow': 1,
+    'no-confusing-arrow': 0,
     // disallow modifying variables that are declared using const
     'no-const-assign': 2,
     // disallow duplicate name in class members
