cec8dd51d0ab43c8a69eb8510a6890374b51ff06
Thomas Thiebaud <thiebaud.tom@gmail.com>
2019-05-29 14:48:03 +0200

    Fix: Use trustpilot prettier rules for typescript preset

--- typescript.js
+++ typescript.js
@@ -4,9 +4,13 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'prettier/@typescript-eslint',
     'plugin:prettier/recommended',
+    'trustpilot/prettier'
   ],
   parserOptions: {
     ecmaVersion: 2018,
     sourceType: 'module',
   },
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 'off'
+  }
 };
